ad00e9c7f7188eebfe8416dd79bd6df0cfb22b6c
Aditya-gth <adityasingheu@gmail.com>
2025-09-06 17:19:57 +0530

    Update vite.config.ts

--- Mindease Mental Wellness App/vite.config.ts
+++ Mindease Mental Wellness App/vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base:'/mindease-app/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
